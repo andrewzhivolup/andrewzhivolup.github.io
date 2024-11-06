bdcb43f26e082dc4b68a3906b785f1cd39be73ba
andrewzhivolup <ferari1190@mail.ru>
2024-11-06 19:25:39 +0300

    feat: create react app

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <body>
+          <header>
+            <div class="logo">Минимална Ана</div>
+            <h1>Дизайн клир</h1>
+            <nav>
+              <ul>
+                <li>
+                  <a href="#">Образование</a>
+                </li>
+                <li>
+                  <a href="#">Контакты</a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <main>
+            <div class="product">
+              <img
+                src="https://source.unsplash.com/1600x900/?fashion"
+                alt="Fashion Collection"
+              />
+              <p>ТРОЙТИ НА ШАБЛА ВОР</p>
+            </div>
+          </main>
+          <footer>
+            <a href="#">Минимална Ана</a>
+            <a href="#">Образование</a>
+            <a href="#">Контакты</a>
+          </footer>
+        </body>
+      </div>
+    </>
+  );
+}
+
+export default App;
