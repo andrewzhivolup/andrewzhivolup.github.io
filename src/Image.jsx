62e86b20cef10a3ff0be19a46a55ee6d0fd669f3
andrewzhivolup <ferari1190@mail.ru>
2024-11-19 23:00:00 +0300

    feat: adde image and image group

---
+++ src/Image.jsx
@@ -0,0 +1,59 @@
+import { ZoomInOutlined, ZoomOutOutlined } from "@ant-design/icons";
+import { Image, Space } from "antd";
+import React from "react";
+
+function _Image({ src, className }) {
+  return (
+    <div className={className}>
+      <Image
+        src={src}
+        className={className}
+        preview={{
+          mask: (
+            <div style={{ fontSize: 24, color: "white" }}>
+              Предварительный просмотр
+            </div>
+          ),
+        }}
+      />
+    </div>
+  );
+}
+function ImageGroup({ children }) {
+  return (
+    <Image.PreviewGroup
+      preview={{
+        toolbarRender: (
+          _,
+          { transform: { scale }, actions: { onZoomOut, onZoomIn } }
+        ) => (
+          <div
+            style={{
+              backgroundColor: "rgba(0, 0, 0, 0.3)",
+              padding: "15px 30px",
+              borderRadius: "30px",
+              color: "white",
+            }}
+          >
+            <Space size={12} className="toolbar-wrapper">
+              <ZoomOutOutlined
+                style={{ fontSize: "30px" }}
+                disabled={scale === 1}
+                onClick={onZoomOut}
+              />
+              <ZoomInOutlined
+                style={{ fontSize: "30px" }}
+                disabled={scale === 50}
+                onClick={onZoomIn}
+              />
+            </Space>
+          </div>
+        ),
+      }}
+    >
+      {children}
+    </Image.PreviewGroup>
+  );
+}
+
+export { ImageGroup, _Image };
