41de7966691bb33a986067d08afb83d363306ceb
andrewzhivolup <ferari1190@mail.ru>
2024-11-21 22:47:18 +0300

    fix: colors on black theme

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,14 @@
 import { Tooltip } from "antd";
 
+import "./Footer.scss";
+
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   const version = process.env.REACT_APP_VERSION || "7.1.3";
 
   return (
     <footer>
-      <p className="ibm-plex-sans-regular">
+      <p className={`ibm-plex-sans-regular `}>
         {currentYear}, Маминова Анна
         <Tooltip placement="bottom" title={`Версия сайта: ${version}`}>
           {" ©"}
